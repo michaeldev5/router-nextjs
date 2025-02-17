1d2b13748608e3a5cf923d179b785c14360339e9
Michael Absolon <absolon.mm@gmail.com>
2025-02-17 22:30:50 +0100

    Add XCM Router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,65 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { RouterBuilder } from "@paraspell/xcm-router";
+import { useEffect, useState } from "react";
+import {
+  web3Accounts,
+  web3Enable,
+  web3FromAddress,
+} from "@polkadot/extension-dapp";
+import { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
 
 export default function Home() {
+  const [account, setAccount] = useState<InjectedAccountWithMeta | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const onSwap = async () => {
+    if (!account) {
+      return;
+    }
+
+    const injector = await web3FromAddress(account?.address);
+
+    setLoading(true);
+
+    await RouterBuilder()
+      .from("Hydration")
+      .exchange("HydrationDex")
+      .to("Hydration")
+      .currencyFrom({ symbol: "DOT" })
+      .currencyTo({ symbol: "HDX" })
+      .amount(BigInt(100000000))
+      .senderAddress(account.address)
+      .recipientAddress("7LR8z5RhCWwweSUqomH68fn63e5CbSqAoLAU8qQM7gxXs7Mk")
+      .signer(injector.signer)
+      .slippagePct("0.1")
+      .build();
+
+    setLoading(false);
+  };
+
+  const init = async () => {
+    const allInjected = await web3Enable("my cool dapp");
+
+    if (allInjected.length === 0) {
+      console.log("no injected sources available");
+    }
+
+    const allAccounts = await web3Accounts();
+
+    if (allAccounts.length === 0) {
+      console.log("no accounts available");
+    }
+
+    setAccount(allAccounts[0]);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -13,82 +71,11 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <button style={{ padding: 24, cursor: "pointer" }} onClick={onSwap}>
+          {loading ? "Loading..." : "Swap"}
+        </button>
       </footer>
     </div>
   );
